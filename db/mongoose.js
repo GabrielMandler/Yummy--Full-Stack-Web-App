b9607deb8b36b5dae5d216ca2697b6e6e3269634
Gabriel <gabrimandler@gmail.com>
2018-11-13 01:02:41 +0200

    Initial commit

--- db/mongoose.js
+++ db/mongoose.js
@@ -1,6 +1,7 @@
 var mongoose = require('mongoose');
+const MONGODB_URI= "mongodb://gabriel:rchbjni123@ds145463.mlab.com:45463/web-project";
 
 mongoose.Promise = global.Promise;
-mongoose.connect(process.env.MONGODB_URI);
+mongoose.connect(MONGODB_URI);
 
 module.exports = {mongoose};
