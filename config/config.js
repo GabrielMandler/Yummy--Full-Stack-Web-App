783857173ad5fc39f1005d387026c808cd1ef2b9
Gabriel <gabrimandler@gmail.com>
2018-11-13 13:13:49 +0200

    Initial commit

--- config/config.js
+++ config/config.js
@@ -1,6 +1,6 @@
 var env = process.env.NODE_ENV || 'development';
 
-if (env === 'development' || env === 'test') {
+if (env === 'production' || env === 'development' || env === 'test') {
   var config = require('./config.json');
   var envConfig = config[env];
 
