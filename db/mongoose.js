c30548837ee6b11dd588ea08cf88172bb71e92f9
Gabriel <gabrimandler@gmail.com>
2018-11-13 11:10:32 +0200

    Initial commit

--- db/mongoose.js
+++ db/mongoose.js
@@ -1,6 +1,10 @@
 var mongoose = require('mongoose');
 
 mongoose.Promise = global.Promise;
-mongoose.connect(process.env.MONGOLAB_URI);
+mongoose.connect(process.env.MONGOLAB_URI, { 
+    uri_decode_auth: true 
+    }, function(err, db) {
+
+    });
 
 module.exports = {mongoose};
