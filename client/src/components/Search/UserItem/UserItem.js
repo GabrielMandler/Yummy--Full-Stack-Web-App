79bea51938461152974ecf2723eeaf5b7364cbfd
Gabriel <gabrimandler@gmail.com>
2018-11-14 21:05:15 +0200

    'initialCommit'

--- client/src/components/Search/UserItem/UserItem.js
+++ client/src/components/Search/UserItem/UserItem.js
@@ -1,13 +1,15 @@
 import React, { Component } from 'react';
 import { Link } from "react-router-dom";
 
+import classes from './UserItem.css';
+
 const UserItem = (props) => {
     let usersLink = 'users?id='+ props.id;
 
     return (
         <Link to= {usersLink}>
             <li>
-                <img src={props.image} /> 
+                <img src={props.image} className={classes.img} /> 
                 <h3> {props.name} </h3>
             </li>
         </Link>
