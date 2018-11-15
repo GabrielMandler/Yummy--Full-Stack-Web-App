54b0f737247bfd393500e1ec642254ceeca5aa99
Gabriel <gabrimandler@gmail.com>
2018-11-15 14:34:33 +0200

    'initialCommit'

--- middleware/imageUpload.js
+++ middleware/imageUpload.js
@@ -55,7 +55,7 @@ ImageUpload.uploadToGcs = (req, res, next) => {
   folderExist("bitx")
     .then((doesExist) => {
       if(!doesExist){
-        folderName = userId + FOLDER_SUFFIX;
+        folderName = userId;
         console.log("does exist return value: " ,doesExist, "foldername: " ,folderName);
         createFolders(FOLDER_SUFFIX)
         .then(() => {
