11c989092395355c49eeb0d787c74d2e451293aa
Gabriel <gabrimandler@gmail.com>
2018-11-16 15:53:25 +0200

    'initialCommit'

--- client/src/store/actions/auth.js
+++ client/src/store/actions/auth.js
@@ -62,6 +62,7 @@ export const auth = (authData, isSignup) =>{
                 dispatch(checkAuthTimeout(expiresIn));
             })
             .catch(err => {
+                console.log(err);
                 dispatch(authFail(err));
             });
     };
