c8cec5b7d4092efe1b62f40341c85fcffdc4fcbf
Gabriel <gabrimandler@gmail.com>
2018-11-16 16:00:47 +0200

    'initialCommit'

--- controllers/auth.js
+++ controllers/auth.js
@@ -12,6 +12,7 @@ exports.login = (req, res) => {
             res.status(200).header({'token': token,'expiresin': process.env.TOKEN_EXPIRES_IN}).send(user);
           })
         }).catch( (err) =>{
+          console.log(err);
           res.status(400).json(err);
       })
   
