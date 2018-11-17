2884728952251dcc92f4fbaabfef863b8f7f4d90
Gabriel <gabrimandler@gmail.com>
2018-11-17 18:44:57 +0200

    'initialCommit'

--- client/src/components/Navigation/Toolbar/Toolbar.js
+++ client/src/components/Navigation/Toolbar/Toolbar.js
@@ -6,7 +6,7 @@ import NavigationItems from '../NavigationItems/NavigationItems';
 import DrawerToggle from '../SideDrawer/DrawerToggle/DrawerToggle';
 import * as Resources from '../../../shared/resources';
 
-let height = "100px";
+let height = "50px";
 
 const toolbar = ( props ) => ( 
     
