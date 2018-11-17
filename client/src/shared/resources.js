5a37a6257ebaeffb9e0529233cd6eda37fa59010
Gabriel <gabrimandler@gmail.com>
2018-11-17 18:56:05 +0200

    'initialCommit'

--- client/src/shared/resources.js
+++ client/src/shared/resources.js
@@ -1,5 +1,5 @@
-export const user = './assets/user.png';
-export const users = './assets/users.png';
-export const newpost = './assets/newpost.png';
-export const logo = './assets/logo.png';
-export const completeLogo = './assets/logo2.png';
+export const user = 'assets/user.png';
+export const users = 'assets/users.png';
+export const newpost = 'assets/newpost.png';
+export const logo = 'assets/logo.png';
+export const completeLogo = 'assets/logo2.png';
