8b496e8a6fd2955e10a7d156cf9759bf109ca67a
Gabriel <gabrimandler@gmail.com>
2018-11-17 18:49:34 +0200

    'initialCommit'

--- client/src/shared/resources.js
+++ client/src/shared/resources.js
@@ -1,5 +1,5 @@
-export const user = '../../build/static/media/user.png';
-export const users = '../../build/static/media/users.png';
-export const newpost = '../../build/static/media/newpost.png';
-export const logo = '../../build/static/media/logo.png';
-export const completeLogo = '../../build/static/media/logo2.png';
+export const user = '../assets/images/user.png';
+export const users = '../assets/images/users.png';
+export const newpost = '../assets/images/newpost.png';
+export const logo = '../assets/images/logo.png';
+export const completeLogo = '../assets/images/logo2.png';
