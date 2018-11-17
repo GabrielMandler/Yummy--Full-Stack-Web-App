9b8f852988ddc28947d2482ecdcc2cc32b268209
Gabriel <gabrimandler@gmail.com>
2018-11-17 12:16:42 +0200

    'initialCommit'

--- client/src/components/Logo/Logo.js
+++ client/src/components/Logo/Logo.js
@@ -2,8 +2,9 @@ import React from 'react';
 
 import * as Resources from '../../shared/resources';
 import classes from './Logo.css';
-
+{console.log(Resources.logo)}
 const logo = (props) => (
+   
     <div className={classes.Logo} style={{height: props.height}}>
         <img src={Resources.logo} alt="MyBurger" />
          MeetFeed 
--- client/src/shared/resources.js
+++ client/src/shared/resources.js
@@ -1,4 +1,4 @@
-export const user = '../assets/images/user.png';
-export const users = '../assets/images/users.png';
-export const newpost = '../assets/images/newpost.png';
-export const logo = '../assets/images/logo.png';
+export const user = '../../assets/images/user.png';
+export const users = '../../assets/images/users.png';
+export const newpost = '../../assets/images/newpost.png';
+export const logo = '../../assets/images/logo.png';
