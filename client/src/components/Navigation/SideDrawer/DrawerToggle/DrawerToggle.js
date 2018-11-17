280dc0156de7c96457c8e44930ac2035832558ce
Gabriel <gabrimandler@gmail.com>
2018-11-18 00:43:25 +0200

    'initialCommit'

--- client/src/components/Navigation/SideDrawer/DrawerToggle/DrawerToggle.js
+++ client/src/components/Navigation/SideDrawer/DrawerToggle/DrawerToggle.js
@@ -3,7 +3,7 @@ import React from 'react';
 import classes from './DrawerToggle.css';
 
 const drawerToggle = (props) => (
-    <div className={classes.DrawerToggle} onClick={props.clicked} id="nav-close-icon">
+    <div className={classes.DrawerToggle} onClick={props.clicked} >
         <span></span>
         <span></span>
         <span></span>
