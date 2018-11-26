9152f98c3dcd9a002d65ddaa78872fc42462fd1c
Gabriel <gabrimandler@gmail.com>
2018-11-27 01:12:12 +0200

    'initialCommit'

--- client/src/shared/resources.js
+++ client/src/shared/resources.js
@@ -1,5 +1,5 @@
-export const user = 'https://storage.googleapis.com/staging.webproject-cd3b2.appspot.com/assets/user.png';
-export const users = 'https://storage.googleapis.com/staging.webproject-cd3b2.appspot.com/assets/users.png';
-export const newpost = 'https://storage.googleapis.com/staging.webproject-cd3b2.appspot.com/assets/newpost.png';
+export const user = 'https://storage.googleapis.com/staging.webproject-cd3b2.appspot.com/assets/user.jpeg';
+export const users = 'https://storage.googleapis.com/staging.webproject-cd3b2.appspot.com/assets/users.jpeg';
+export const newpost = 'https://storage.googleapis.com/staging.webproject-cd3b2.appspot.com/assets/newpost.jpeg';
 export const logo = 'https://storage.googleapis.com/staging.webproject-cd3b2.appspot.com/assets/logo.png';
 export const completeLogo = 'https://storage.googleapis.com/staging.webproject-cd3b2.appspot.com/assets/logo2.png';
