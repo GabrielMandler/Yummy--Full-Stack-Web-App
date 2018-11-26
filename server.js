466b424731f9dc1d3ce3f1d8103d0b75eb8f4ada
Gabriel <gabrimandler@gmail.com>
2018-11-27 00:34:14 +0200

    'initialCommit'

--- server.js
+++ server.js
@@ -17,7 +17,7 @@ app.use(bodyParser.urlencoded({
 app.use(bodyParser.json());
 
 app.use(express.static(path.resolve(__dirname, "client", "build")));
-app.get("/", (req, res) => {
+app.get("/*", (req, res) => {
   res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
 });
 
