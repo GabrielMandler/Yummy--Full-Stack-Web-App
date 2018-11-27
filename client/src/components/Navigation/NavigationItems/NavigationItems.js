dfc3c8db750e74dad96d47aeeab67274af3157d2
Gabriel <gabrimandler@gmail.com>
2018-11-28 00:32:50 +0200

    'initialCommit'

--- client/src/components/Navigation/NavigationItems/NavigationItems.js
+++ client/src/components/Navigation/NavigationItems/NavigationItems.js
@@ -15,7 +15,7 @@ const navigationItems = (props) => {
             <NavigationItem link="/search" exact clicked={props.clicked} icon={Resources.iconSearch} alt="Search"></NavigationItem>
             <NavigationItem link={usersLink} exact clicked={props.clicked} icon={Resources.iconUser} alt="Profile"></NavigationItem>
             <span className={classes.SubMenu}>
-                <NavigationItem link="/newPost" clicked={props.clicked} icon={Resources.iconAddNew} alt="Add new post!">!</NavigationItem>
+                <NavigationItem link="/newPost" clicked={props.clicked} icon={Resources.iconAddNew} alt="Add new post!"></NavigationItem>
                 <NavigationItem link="/Logout" clicked={props.clicked} icon={Resources.iconSignout} alt="Logout"></NavigationItem>
             </span>
         </ul>
