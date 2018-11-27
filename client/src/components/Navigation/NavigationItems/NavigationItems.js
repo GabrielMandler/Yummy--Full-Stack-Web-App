7ac0001eee6c946b23eb6104eb4ecb096285a59c
Gabriel <gabrimandler@gmail.com>
2018-11-28 00:21:49 +0200

    'initialCommit'

--- client/src/components/Navigation/NavigationItems/NavigationItems.js
+++ client/src/components/Navigation/NavigationItems/NavigationItems.js
@@ -11,12 +11,12 @@ const navigationItems = (props) => {
     return (
         <Aux>
         <ul className={classes.NavigationItems}>
-            <NavigationItem link="/" exact clicked={props.clicked} icon={Resources.iconHome} alt="Feed">Feed</NavigationItem>
-            <NavigationItem link="/search" exact clicked={props.clicked} icon={Resources.iconSearch} alt="Search">Search</NavigationItem>
-            <NavigationItem link={usersLink} exact clicked={props.clicked} icon={Resources.iconUser} alt="Profile">Profile</NavigationItem>
+            <NavigationItem link="/" exact clicked={props.clicked} icon={Resources.iconHome} alt="Feed"></NavigationItem>
+            <NavigationItem link="/search" exact clicked={props.clicked} icon={Resources.iconSearch} alt="Search"></NavigationItem>
+            <NavigationItem link={usersLink} exact clicked={props.clicked} icon={Resources.iconUser} alt="Profile"></NavigationItem>
             <span className={classes.SubMenu}>
-                <NavigationItem link="/newPost" clicked={props.clicked} icon={Resources.iconAddNew} alt="Add new post!">Add new post!</NavigationItem>
-                <NavigationItem link="/Logout" clicked={props.clicked} icon={Resources.iconSignout} alt="Logout">Logout</NavigationItem>
+                <NavigationItem link="/newPost" clicked={props.clicked} icon={Resources.iconAddNew} alt="Add new post!">!</NavigationItem>
+                <NavigationItem link="/Logout" clicked={props.clicked} icon={Resources.iconSignout} alt="Logout"></NavigationItem>
             </span>
         </ul>
         </Aux>
